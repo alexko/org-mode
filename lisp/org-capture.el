4bdf7444406275e08df4394dd69ae46318758a85
alexko <alex@3form.com>
2012-06-24 14:58:15 -0700

    temp fix for org-capture not restoring the location of the point

--- lisp/org-capture.el
+++ lisp/org-capture.el
@@ -584,7 +584,9 @@ of the day at point (if any) or the current HH:MM time."
 			     (org-current-time)))
 	(org-capture-set-target-location)
 	(condition-case error
-	    (org-capture-put :template (org-capture-fill-template))
+	    (org-capture-put :template
+			     (save-excursion
+			       (org-capture-fill-template)))
 	  ((error quit)
 	   (if (get-buffer "*Capture*") (kill-buffer "*Capture*"))
 	   (error "Capture abort: %s" error)))
